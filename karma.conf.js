1bab94e5c23d10cf1bac093b5f64dc4d1a72fcac
stelios kontarinis <stelios.kontarinis@eurodyn.com>
2020-10-13 14:13:25 +0300

    jenkins update again

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    customLaunchers: {
+      chromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true,
     restartOnFileChange: true
   });
 };
