8cf66d50af42d45913fcc701cd1f24144dc78674
stelios kontarinis <stelios.kontarinis@eurodyn.com>
2020-10-13 14:54:23 +0300

    karma.conf update

--- karma.conf.js
+++ karma.conf.js
@@ -25,11 +25,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
     browsers: ['Chrome','ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: [
+          '--disable-web-security',
+          '--disable-gpu',
+          '--no-sandbox'
+        ]
       }
     },
     singleRun: false,
